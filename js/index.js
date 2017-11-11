9143879d011e9e303cf5990ac0f6fd9544f18c94
Sol Kim <solkim813@gmail.com>
2017-11-11 18:44:38 +0000

    attempt to remove fouc

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-// SHOW CAROUSEL AFTER ALL IMGS LOADED
-$('.photos').imagesLoaded().done( function() {
-//	$('#placeholder').hide();
-//	$('.photos').show(300);
-//	$('.photos').slick('slickPlay');
+// PHOTO CAROUSEL
+$(document).ready(function(){
 	$('.photos').slick({
 		accessibility: true,
 		autoplay: true,
@@ -19,22 +16,3 @@ $('.photos').imagesLoaded().done( function() {
 		variableWidth: true,
 	});
 });
-
-//// PHOTO CAROUSEL
-//$(document).ready(function(){
-//	$('.photos').slick({
-//		accessibility: true,
-//		autoplay: false,
-////		autoplaySpeed: 2500,
-//		arrows: true,
-//		centerMode: true,
-//		centerPadding: '10px',
-//		focusOnSelect: true,
-//		initialSlide: 0,
-//		pauseOnHover: false,
-//		slidesToShow: 1,
-//		speed: 1000,
-//		swipeToSlide: true,
-//		variableWidth: true,
-//	});
-//});
