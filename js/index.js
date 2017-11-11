00eb6ea4520bb8195a30ffd47315fa705a1b4a32
Sol Kim <solkim813@gmail.com>
2017-11-11 18:18:56 +0000

    remove unnecessary file downloads

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-// PHOTO CAROUSEL
-$(document).ready(function(){
+// SHOW CAROUSEL AFTER ALL IMGS LOADED
+$('.photos').imagesLoaded().done( function() {
+//	$('#placeholder').hide();
+//	$('.photos').show(300);
+//	$('.photos').slick('slickPlay');
 	$('.photos').slick({
 		accessibility: true,
 		autoplay: true,
@@ -8,18 +11,30 @@ $(document).ready(function(){
 		centerMode: true,
 		centerPadding: '10px',
 		focusOnSelect: true,
-		slidesToShow: 3,
+		initialSlide: 0,
+		pauseOnHover: false,
+		slidesToShow: 1,
 		speed: 1000,
 		swipeToSlide: true,
 		variableWidth: true,
-//		responsive: [
-//			{
-//				breakpoint: 450,
-//				settings: {
-//					slidesToShow: 1,
-//					slidesToScroll: 1
-//				}
-//			},
-//		]
 	});
 });
+
+//// PHOTO CAROUSEL
+//$(document).ready(function(){
+//	$('.photos').slick({
+//		accessibility: true,
+//		autoplay: false,
+////		autoplaySpeed: 2500,
+//		arrows: true,
+//		centerMode: true,
+//		centerPadding: '10px',
+//		focusOnSelect: true,
+//		initialSlide: 0,
+//		pauseOnHover: false,
+//		slidesToShow: 1,
+//		speed: 1000,
+//		swipeToSlide: true,
+//		variableWidth: true,
+//	});
+//});
